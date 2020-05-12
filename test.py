3f38e5fdb6416c9955d0039c123f270c00259d5e
mpiwonski <marcin6207@gmail.com>
2020-05-12 13:54:58 +0200

    Kolejna edycja

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import unittest
+from fractions import Fraction
+
 
 class TestSum(unittest.TestCase):
     def test_list_int(self):
@@ -6,6 +8,13 @@ class TestSum(unittest.TestCase):
         result = sum(data)
         self.assertEquals(result, 6)
         print('Chyba się udało!')
+
+    def test_list_fraction(self):
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        result = sum(data)
+        self.assertEquals(result, 1)
+
+
 
 if __name__ == 'main':
     unittest.main()
